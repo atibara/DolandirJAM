b9d6fd0a73956d51f2e7a1b2b31da115e50c3833
cioccolata1113 <tansonat12@gmail.com>
2025-12-06 15:48:20 +0300

    ..

--- Assets/SONAT/TopDownController.cs
+++ Assets/SONAT/TopDownController.cs
@@ -2,15 +2,65 @@ using UnityEngine;
 
 public class TopDownController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 10f; // Dönüþ yumuþaklýðý
+
+    private Rigidbody2D rb;
+    private Vector2 movementInput;
+    private Vector2 smoothedMovementInput;
+    private Vector2 movementInputSmoothVelocity;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Inputlarý her karede alýyoruz
+        ProcessInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        // Fizik iþlemlerini burada yapýyoruz
+        Move();
+        RotateTowardMovement();
+    }
+
+    private void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal"); // A-D veya Sol-Sað Ok
+        float moveY = Input.GetAxisRaw("Vertical");   // W-S veya Yukarý-Aþaðý Ok
+
+        movementInput = new Vector2(moveX, moveY).normalized; // Çaprazda hýzlanmayý engeller
+    }
+
+    private void Move()
+    {
+        // Karakteri hareket ettir
+        rb.linearVelocity = movementInput * moveSpeed;
+        // Not: Unity 6'da 'velocity' yerine 'linearVelocity' tavsiye edilir, 
+        // eðer eski sürümden uyarý alýrsan burayý 'rb.velocity' yapabilirsin.
+    }
+
+    private void RotateTowardMovement()
+    {
+        // Eðer karakter hareket ediyorsa
+        if (movementInput != Vector2.zero)
+        {
+            // Hareket yönüne göre açýyý hesapla
+            float targetAngle = Mathf.Atan2(movementInput.y, movementInput.x) * Mathf.Rad2Deg;
+
+            // Sprite'ýnýn varsayýlan yönü SAÐ (Right) ise -90 çýkarmana gerek yok.
+            // Eðer Sprite'ýn YUKARI bakýyorsa, (targetAngle - 90) yapmalýsýn.
+            // Genelde Unity 2D'de "Sað" 0 derecedir.
+
+            Quaternion targetRotation = Quaternion.Euler(0, 0, targetAngle);
+
+            // Yumuþak dönüþ için Slerp kullanýyoruz
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+        }
     }
 }
